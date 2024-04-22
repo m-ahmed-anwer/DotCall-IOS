71c6f56b2f33cca0dcc18f05c5b98487f106f576
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-04-22 10:29:10 +0530

    Initial Commit

---
+++ DotCall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DotCall
+//
+//  Created by Ahmed Anwer on 2024-04-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
